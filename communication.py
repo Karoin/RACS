756dce0825db57ccc48eebd60eaffe384ce0ed2c
Karoin <47442515+Karoin@users.noreply.github.com>
2020-08-18 11:35:45 +0900

    Create communication.py

---
+++ communication.py
@@ -0,0 +1,14 @@
+# SOCKET COMMUNICATION
+'''
+To communicate with clients as SERVER, 
+you are asked to set up 'IP address' and 'Port Number'.
+'''
+
+from communication_structure import do_threading
+
+
+
+commThID = thread_run(do_threading, loop=True)
+
+
+server_socket_close(socket)

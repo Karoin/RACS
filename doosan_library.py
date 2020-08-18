f03ec377cff080368118d906e4b5db83a9a3b284
Karoin <47442515+Karoin@users.noreply.github.com>
2020-08-18 11:35:28 +0900

    Create doosan_library.py

---
+++ doosan_library.py
@@ -0,0 +1,84 @@
+# Temporary value assignment for detecting error
+DR_BASE = 1
+DR_HOLD = 0
+ON = 1
+OFF = 0
+Global_g_combo1 = 0
+Global_g_combo2 = 0
+Global_g_combo3 = 0
+Global_g_combo4 = 0
+Global_g_combo5 = 0
+Global_g_combo6 = 0
+Global_g_chademo1 = 0
+Global_g_chademo2 = 0
+Global_g_chademo3 = 0
+Global_g_chademo4 = 0
+Global_g_chademo5 = 0
+Global_g_chademo6 = 0
+chademo = 0
+combo = 0
+normal = 0
+Global_t_combo1 = 0
+Global_t_combo2 = 0
+Global_t_combo3 = 0
+Global_t_combo4 = 0
+Global_t_combo5 = 0
+Global_t_combo6 = 0
+Global_t_chademo1 = 0
+Global_t_chademo2 = 0
+Global_t_chademo3 = 0
+Global_t_chademo4 = 0
+Global_t_chademo5 = 0
+Global_t_chademo6 = 0
+
+
+def server_socket_open(a):
+    return a
+
+def server_socket_read(a, length=-1, timeout=-1):
+    b = a
+    return a, b
+
+def server_socket_write(a, b):
+    pass
+
+def server_socket_state(a):
+    return a
+
+def server_socket_close(a):
+    return a
+
+def get_current_posx(ref=-1):
+    a = 1
+    b = 2
+    return a, b
+
+def posx(a):
+    return a
+
+def thread_run(a, loop=True):
+    return a
+
+def set_tool_digital_output(a, b):
+    a = b
+
+def get_tool_digital_input(a):
+    return a
+
+def stop(a):
+    pass
+
+def movel(a, time=5):
+    pass
+
+def sub_program_run(a):
+    pass
+
+def exit():
+    pass
+
+def set_tool(normal):
+    pass
+
+def wait(a):
+    pass
